30dab270b839674114e7d901783455a11a3b0c13
shahafSegal <goigadol360@gmail.com>
2024-02-22 12:26:01 +0200

    added search to products

--- routes/products.routes.js
+++ routes/products.routes.js
@@ -2,11 +2,10 @@ const express=require('express')
 const router=express.Router();
 const { auth } = require('../middlewares/auth');
 const newProduct = require('../controllers/Products/newProduct');
+const searchProduct = require('../controllers/Products/searchProduct');
 
 
-router.post("/new",newProduct)
-
-
-
+router.post("/new",auth,newProduct)
+router.get("/search",auth,searchProduct)
 
 module.exports=router;
