405c8a4c73f57853e77277373b15a6fa5d362e16
shahafSegal <goigadol360@gmail.com>
2024-02-22 12:24:40 +0200

    added update to prices

---
+++ controllers/Products/UpdatePrices.js
@@ -0,0 +1,32 @@
+const Product = require("../../models/products.model")
+
+const updateProductPrice= async(productID)=>{
+    try{
+        const product= await Product.findById(productID).populate("listings")
+        let minPrice = 0;
+        let maxPrice = 0;
+
+        if (product.listings.length !== 0) {
+            minPrice = product.listings[0].price;
+            maxPrice = product.listings[0].price;
+
+            product.listings.forEach(({ price }) => {
+                if (price > maxPrice) {
+                    maxPrice = price;
+                } else if (price < minPrice) {
+                    minPrice = price;
+                }
+            });
+        }
+         
+        product.max_price=maxPrice;
+        product.min_price=minPrice;
+        await product.save()
+        console.log(product)
+
+    }catch(err){
+        console.log(err)
+    }
+}
+
+module.exports=updateProductPrice
