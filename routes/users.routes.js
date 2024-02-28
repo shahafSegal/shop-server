401c13be151875496f797412d1d56a057a09e2c6
shahafSegal <goigadol360@gmail.com>
2024-02-28 12:43:07 +0200

    added role change

--- routes/users.routes.js
+++ routes/users.routes.js
@@ -2,7 +2,7 @@ const express=require('express')
 const router=express.Router();
 const { registerUser } = require('../controllers/users/register');
 const { loginUser } = require('../controllers/users/login');
-const { changePassword, deleteUser } = require('../controllers/users');
+const { changePassword, deleteUser, changeUserSetting } = require('../controllers/users');
 const { auth } = require('../middlewares/auth');
 const { getUser } = require('../controllers/users/getUser');
 
@@ -15,7 +15,9 @@ router.get('/login',auth,getUser)
 
 router.patch('/password/:id',auth,changePassword)
 
-router.delete('/remove/:id',auth,deleteUser)
+router.get('/roleChange',auth,changeUserSetting)
+
+// router.delete('/remove/:id',auth,deleteUser)
 
 
 module.exports=router;
